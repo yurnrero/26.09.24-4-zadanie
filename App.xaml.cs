10173c806868da3cbec0482d979cd29d55af8b5d
eltsovya <eltsovya@310D-10>
2024-09-26 11:52:52 +0700

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _26._09._24_4_zadanie
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
